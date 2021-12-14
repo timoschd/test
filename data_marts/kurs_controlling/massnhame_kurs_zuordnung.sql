93d55981d7c6745bddea58e692e4f71f2f386cae
Tim M. Schendzielorz <tim.schendzielorz@googlemail.com>
2021-12-14 09:47:52 +0100

    save commit before merge

--- data_marts/kurs_controlling/massnhame_kurs_zuordnung.sql
+++ data_marts/kurs_controlling/massnhame_kurs_zuordnung.sql
@@ -5,8 +5,8 @@ WITH temptable AS (
             massnahmen_organisation_courses.app_item_id_formatted AS massnahmen_id_qm,
             massnahmen_organisation_courses.titel AS massnahmen_titel,
             json_array_elements((massnahmen_organisation_courses.json_coursedetails_new::json -> 0) -> 'Komponenten'::text) AS lehrgangsdetails,
-	 		 CONCAT('CRSE', massnahmen_organisation_courses.courses_sales_management::json ->> 'app_item_id') as massnahmen_id
-           FROM podio.massnahmen_organisation_courses
+            CONCAT('CRSE', massnahmen_organisation_courses.courses_sales_management::json ->> 'app_item_id') as massnahmen_id
+            FROM podio.massnahmen_organisation_courses
         ), temptable_2 AS (
          SELECT temptable.app_item_id,
 			temptable.massnahmen_id_qm,
@@ -17,18 +17,18 @@ WITH temptable AS (
             temptable.lehrgangsdetails ->> 'roleTitle'::text AS lehrgang_fachbereich,
             (temptable.lehrgangsdetails ->> 'Dauer'::text)::double precision AS lehrgang_dauer_in_wochen,
             row_number() OVER (PARTITION BY temptable.app_item_id) AS lehrgang_reihenfolge
-           FROM temptable
+            FROM temptable
         )
- SELECT temptable_2.app_item_id,
- 	temptable_2.massnahmen_id_qm,
-    temptable_2.massnahmen_id,
-    temptable_2.massnahmen_titel,
-    temptable_2.lehrgang_titel,
-    temptable_2.lehrgang_id,
-    temptable_2.lehrgang_fachbereich,
-    temptable_2.lehrgang_dauer_in_wochen,
-    temptable_2.lehrgang_reihenfolge,
-    sum(temptable_2.lehrgang_dauer_in_wochen) OVER (PARTITION BY temptable_2.app_item_id ORDER BY temptable_2.lehrgang_reihenfolge) AS lehrgang_dauer_in_wochen_sum
+SELECT temptable_2.app_item_id,
+   temptable_2.massnahmen_id_qm,
+   temptable_2.massnahmen_id,
+   temptable_2.massnahmen_titel,
+   temptable_2.lehrgang_titel,
+   temptable_2.lehrgang_id,
+   temptable_2.lehrgang_fachbereich,
+   temptable_2.lehrgang_dauer_in_wochen,
+   temptable_2.lehrgang_reihenfolge,
+   sum(temptable_2.lehrgang_dauer_in_wochen) OVER (PARTITION BY temptable_2.app_item_id ORDER BY temptable_2.lehrgang_reihenfolge) AS lehrgang_dauer_in_wochen_sum
    FROM temptable_2;
    
 -- Set indices
