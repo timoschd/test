b5fbce95cd50aca84b19c03f4633719251d213b2
oliverwzl <oliver.wenzlaff44@gmail.com>
2021-12-28 13:19:32 +0100

    add buecher_kurs_zuordnung

---
+++ data_marts/kurs_controlling/buecher_kurs_zuordnung.sql
@@ -0,0 +1,37 @@
+--Create Table Bücher-Kurs-Zuordnung
+Create TABLE kc.buecher_kurs_zuordnung AS(
+SELECT
+	app_item_id as buch_id_qm,
+	item_id as buch_id,
+	titel as buch_titel,
+	lgb_title as lgb_titel,
+	cast(json_book as json)->>'author' as autor,
+	(auflage_2::numeric)::integer as auflage,
+	(jahr_2::numeric)::integer as jahr,
+	isbn_2 as isbn,
+	kosten_fachliteratur::numeric as buch_kosten,
+	(cast(gultig_ab as json)->>'start_date')::date as buch_gultig_ab,
+	(cast(gultig_bis as json)->>'start_date')::date as buch_gultig_bis,
+	unnest(string_to_array(qm_ffmt_cmt_ids,','::text))::integer as kurs_id_qm,
+	lizenzkosten as buch_lizenzkosten,--kein cast auf nummeric möglich -> bsp:(730,00€ zzgl. MwSt.), buch_nr: 474
+	(cast(lizenz_gultigkeit as json)->>'start_date')::date as lizenz_gueltig_ab,
+	(cast(lizenz_gultig_bis as json)->>'start_date')::date as lizenz_gueltig_bis,
+	cast(lizenzbereiche as json)->>'text' as lizenzbereiche,
+	cast(lizenzart as json)->>'text' as lizenzart,
+	lizenz_anmerkung as anmerkungen_zur_lizenz
+FROM podio.qs_bucherliste);
+
+-- SET INDICIES & Create PRIMARY KEY 
+ALTER TABLE kc.buecher_kurs_zuordnung ADD COLUMN id SERIAL PRIMARY KEY;
+
+CREATE INDEX ON kc.buecher_kurs_zuordnung (buch_id_qm);
+CREATE INDEX ON kc.buecher_kurs_zuordnung (buch_id;
+
+-- Set forgein constraints
+ALTER TABLE kc.buecher_kurs_zuordnung
+ADD CONSTRAINT fk_kurs
+FOREIGN KEY (kurs_id_qm)
+REFERENCES kc.kurse (kurs_id_qm);
+
+-- Set table owner
+ALTER TABLE kc.buecher_kurs_zuordnung OWNER TO read_only;
