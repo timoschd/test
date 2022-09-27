c9f94859ed21c6482dbab5fedcfa55871c1a11a5
Tim M. Schendzielorz <tim.schendzielorz@googlemail.com>
2022-09-27 16:12:15 +0200

    changed naming, datatypes and column order zoho tickets

--- data_marts/teilnehmer/tickets_zoho.sql
+++ data_marts/teilnehmer/tickets_zoho.sql
@@ -2,17 +2,18 @@
 
 CREATE TABLE tc.tickets_zoho AS --ids
 
-SELECT id AS ticket_id,
+SELECT
+	id AS ticket_id::int,
 	request_id,
 	substring(lead, ('\d+$'))::integer AS lead_id_podio,
 	substring(fulfillment_übersicht, ('\d+$'))::integer AS teilnehmer_id_backoffice_podio,
 	substring(teilnehmer, ('\d+$'))::integer AS teilnehmer_id_tutoren_podio, --ticket daten
  ticket_owner_name AS bearbeiter,
 	erstellungszeit::timestamp AS erstellungsdatum,
-	modifiziert_zeit,	
-	faelligkeitsdatum,
+	modifiziert_zeit::timestamp,	
+	fälligkeitsdatum as faelligkeitsdatum,
 	status,
-	prioritaet,
+	priorität as prioritaet,
 	sprache,
 	quelle, --problem
  department_name AS abteilung,
@@ -30,23 +31,23 @@ SELECT id AS ticket_id,
 	vd___unterkategorien, --kontaktdaten
  kanal,
 	kontakt_id AS kontakt_id_zoho, --zeitdaten
- 	CASE WHEN anzahl_threads = '' THEN NULL ELSE anzahl_threads::numeric END,
-	CASE WHEN assign_time_in_hrs = '' THEN NULL ELSE assign_time_in_hrs::numeric END,
-	CASE WHEN requester_wait_time_in_hrs = '' THEN NULL ELSE requester_wait_time_in_hrs::numeric END,
-	CASE WHEN first_reply_time_in_hrs = '' THEN NULL ELSE first_reply_time_in_hrs::numeric END,
+	customer_response_time::timestamp,
+    CASE WHEN anzahl_threads = '' THEN NULL ELSE anzahl_threads::numeric END,
+	CASE WHEN "assign_time_(hrs)" = '' THEN NULL ELSE "assign_time_(hrs)"::numeric END as assign_time_hrs,
+	CASE WHEN "requester_wait_time_(hrs)" = '' THEN NULL ELSE "requester_wait_time_(hrs)"::numeric END as requester_wait_time_in_hrs,
+	CASE WHEN "first_reply_time_(hrs)" = '' THEN NULL ELSE "first_reply_time_(hrs)"::numeric END as first_reply_time_in_hrs,
 	CASE WHEN ticket_age_in_days = '' THEN NULL ELSE ticket_age_in_days::numeric END,
 	CASE WHEN resolution_time = '' THEN NULL ELSE resolution_time::numeric END,
 	CASE WHEN gesamtzeitaufwand = '' THEN NULL ELSE gesamtzeitaufwand::numeric END,
-	CASE WHEN customer_response_time = '' THEN NULL ELSE customer_response_time::timestamp END,
-	CASE WHEN ticket_abschlusszeit = '' THEN NULL ELSE ticket_abschlusszeit::timestamp END,
+	CASE WHEN lösungszeit_in_geschäftszeiten = '' THEN NULL ELSE lösungszeit_in_geschäftszeiten::numeric END as loesungszeit_in_geschaeftszeiten, 
+	CASE WHEN erste_reaktionszeit_in_geschäftszeiten = '' THEN NULL ELSE erste_reaktionszeit_in_geschäftszeiten::numeric END as erste_reaktionszeit_in_geschaeftszeiten,
+	CASE WHEN gesamtreaktionszeit_in_geschäftszeiten = '' THEN NULL ELSE gesamtreaktionszeit_in_geschäftszeiten::numeric END as gesamtreaktionszeit_in_geschaeftszeiten,
+	CASE WHEN anzahl_reaktionen = '' THEN NULL ELSE anzahl_reaktionen::numeric END,
+    CASE WHEN ticket_abschlusszeit = '' THEN NULL ELSE ticket_abschlusszeit::timestamp END,
 	CASE WHEN request_reopen_time = '' THEN NULL ELSE request_reopen_time::timestamp END,
 	CASE WHEN assigned_time = '' THEN NULL ELSE assigned_time::timestamp END,
 	CASE WHEN first_assigned_time = '' THEN NULL ELSE first_assigned_time::timestamp END,
 	CASE WHEN agent_antwortzeit = '' THEN NULL ELSE agent_antwortzeit::timestamp END,
-	CASE WHEN loesungszeit_in_geschaeftszeiten = '' THEN NULL ELSE loesungszeit_in_geschaeftszeiten::numeric END,
-	CASE WHEN erste_reaktionszeit_in_geschaeftszeiten = '' THEN NULL ELSE erste_reaktionszeit_in_geschaeftszeiten::numeric END,
-	CASE WHEN gesamtreaktionszeit_in_geschaeftszeiten = '' THEN NULL ELSE gesamtreaktionszeit_in_geschaeftszeiten::numeric END,
-	CASE WHEN anzahl_reaktionen = '' THEN NULL ELSE anzahl_reaktionen::numeric END,
 	ticket_handling_mode
 FROM zoho.desk_tickets
 WHERE substring(teilnehmer, ('\d+$'))::integer IN (SELECT teilnehmer_id_tutoren FROM tc.teilnehmer);
--- data_marts/teilnehmer/triggers/update_trigger_tickets_zoho.sql
+++ data_marts/teilnehmer/triggers/update_trigger_tickets_zoho.sql
@@ -9,7 +9,9 @@ RETURNS trigger AS
     DELETE FROM tc.tickets_zoho
     WHERE ticket_id IN (SELECT id AS ticket_id FROM zoho.desk_tickets
 		WHERE modifiziert_zeit > (SELECT max(modifiziert_zeit) FROM tc.tickets_zoho)
-		OR teilnehmer_id_tutoren_podio NOT IN (SELECT teilnehmer_id_tutoren FROM tc.teilnehmer)			   );
+		OR teilnehmer_id_tutoren_podio NOT IN (SELECT teilnehmer_id_tutoren FROM tc.teilnehmer)			   
+		);
+
     -- UPSERT of newer entries
     INSERT INTO tc.tickets_zoho
     SELECT id AS ticket_id,
@@ -17,15 +19,15 @@ RETURNS trigger AS
 	substring(lead, ('\d+$'))::integer AS lead_id_podio,
 	substring(fulfillment_übersicht, ('\d+$'))::integer AS teilnehmer_id_backoffice_podio,
 	substring(teilnehmer, ('\d+$'))::integer AS teilnehmer_id_tutoren_podio, --ticket daten
- ticket_owner_name AS bearbeiter,
+ 	ticket_owner_name AS bearbeiter,
 	erstellungszeit::timestamp AS erstellungsdatum,
 	modifiziert_zeit,	
-	fälligkeitsdatum,
+	fälligkeitsdatum as faelligkeitsdatum,
 	status,
-	priorität,
+	priorität as prioritaet,
 	sprache,
 	quelle, --problem
- department_name AS abteilung,
+	department_name AS abteilung,
 	klassifizierungen,
 	kategorie,
 	hauptkategorien,
@@ -38,29 +40,29 @@ RETURNS trigger AS
 	as___unterkategorien,
 	vd___hauptkategorien,
 	vd___unterkategorien, --kontaktdaten
- kanal,
+ 	kanal,
 	kontakt_id AS kontakt_id_zoho, --zeitdaten
- 	CASE WHEN anzahl_threads = '' THEN NULL ELSE anzahl_threads::numeric END,
-	CASE WHEN assign_time_in_hrs = '' THEN NULL ELSE assign_time_in_hrs::numeric END,
-	CASE WHEN requester_wait_time_in_hrs = '' THEN NULL ELSE requester_wait_time_in_hrs::numeric END,
-	CASE WHEN first_reply_time_in_hrs = '' THEN NULL ELSE first_reply_time_in_hrs::numeric END,
+ 	customer_response_time::timestamp,
+    CASE WHEN anzahl_threads = '' THEN NULL ELSE anzahl_threads::numeric END,
+	CASE WHEN "assign_time_(hrs)" = '' THEN NULL ELSE "assign_time_(hrs)"::numeric END as assign_time_hrs,
+	CASE WHEN "requester_wait_time_(hrs)" = '' THEN NULL ELSE "requester_wait_time_(hrs)"::numeric END as requester_wait_time_in_hrs,
+	CASE WHEN "first_reply_time_(hrs)" = '' THEN NULL ELSE "first_reply_time_(hrs)"::numeric END as first_reply_time_in_hrs,
 	CASE WHEN ticket_age_in_days = '' THEN NULL ELSE ticket_age_in_days::numeric END,
 	CASE WHEN resolution_time = '' THEN NULL ELSE resolution_time::numeric END,
 	CASE WHEN gesamtzeitaufwand = '' THEN NULL ELSE gesamtzeitaufwand::numeric END,
-	CASE WHEN customer_response_time = '' THEN NULL ELSE customer_response_time::timestamp END,
-	CASE WHEN ticket_abschlusszeit = '' THEN NULL ELSE ticket_abschlusszeit::timestamp END,
+	CASE WHEN lösungszeit_in_geschäftszeiten = '' THEN NULL ELSE lösungszeit_in_geschäftszeiten::numeric END as loesungszeit_in_geschaeftszeiten, 
+	CASE WHEN erste_reaktionszeit_in_geschäftszeiten = '' THEN NULL ELSE erste_reaktionszeit_in_geschäftszeiten::numeric END as erste_reaktionszeit_in_geschaeftszeiten,
+	CASE WHEN gesamtreaktionszeit_in_geschäftszeiten = '' THEN NULL ELSE gesamtreaktionszeit_in_geschäftszeiten::numeric END as gesamtreaktionszeit_in_geschaeftszeiten,
+	CASE WHEN anzahl_reaktionen = '' THEN NULL ELSE anzahl_reaktionen::numeric END,
+    CASE WHEN ticket_abschlusszeit = '' THEN NULL ELSE ticket_abschlusszeit::timestamp END,
 	CASE WHEN request_reopen_time = '' THEN NULL ELSE request_reopen_time::timestamp END,
 	CASE WHEN assigned_time = '' THEN NULL ELSE assigned_time::timestamp END,
 	CASE WHEN first_assigned_time = '' THEN NULL ELSE first_assigned_time::timestamp END,
 	CASE WHEN agent_antwortzeit = '' THEN NULL ELSE agent_antwortzeit::timestamp END,
-	CASE WHEN loesungszeit_in_geschaeftszeiten = '' THEN NULL ELSE loesungszeit_in_geschaeftszeiten::numeric END,
-	CASE WHEN erste_reaktionszeit_in_geschaeftszeiten = '' THEN NULL ELSE erste_reaktionszeit_in_geschaeftszeiten::numeric END,
-	CASE WHEN gesamtreaktionszeit_in_geschaeftszeiten = '' THEN NULL ELSE gesamtreaktionszeit_in_geschaeftszeiten::numeric END,
-	CASE WHEN anzahl_reaktionen = '' THEN NULL ELSE anzahl_reaktionen::numeric END,
 	ticket_handling_mode
 FROM zoho.desk_tickets
 WHERE id NOT IN (SELECT ticket_id FROM tc.tickets_zoho) 
-	AND teilnehmer_id_tutoren_podio IN (SELECT teilnehmer_id_tutoren FROM tc.teilnehmer)
+	AND substring(teilnehmer, ('\d+$'))::integer IN (SELECT teilnehmer_id_tutoren FROM tc.teilnehmer)
 
     ON CONFLICT (ticket_id) 
     DO NOTHING;
