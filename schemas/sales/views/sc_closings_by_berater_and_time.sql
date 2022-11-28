a976117622be708338534399fdac8c59a9fe2f95
oliverwzl <oliver.wenzlaff44@gmail.com>
2022-11-28 17:04:02 +0100

    add images

--- schemas/sales/views/sc_closings_by_berater_and_time.sql
+++ schemas/sales/views/sc_closings_by_berater_and_time.sql
@@ -62,7 +62,11 @@ SELECT 	"Deals"."Owner Name"::text as berater
 	GROUP BY berater
 	HAVING "Deals"."Owner Name"::text NOT IN ('Steffen Deutsch', 'Lars Petersen', 'Peter Langheinrich', 'Romy Kopsch', 'karriere tutor', 
                                               'Kiriaki Orfanidis Corral', 'Test Account', 'Melanie Schnitzler')
-	)
+	),
+images as (
+SELECT 	berater,
+		url
+	FROM sc.images)
 	
 -- Table for all kpis
 
@@ -72,14 +76,15 @@ SELECT	berater.berater,
 		COALESCE(month.closing_monat, 0) as closings_monat,
 		COALESCE(year.closing_year, 0) as closings_jahr,
 		COALESCE(stage."75er_gesamt", 0) as "closings_75_stage_gesamt",
-		COALESCE(stage_closing."75er_stage_mit_closing_date_diesen_monat", 0) as "75er_stage_mit_closing_date_diesen_monat"
+		COALESCE(stage_closing."75er_stage_mit_closing_date_diesen_monat", 0) as "75er_stage_mit_closing_date_diesen_monat",
+		images.url
 FROM berater
 	LEFT JOIN today on berater.berater = today.berater
 	LEFT JOIN week on berater.berater = week.berater
 	LEFT JOIN month on berater.berater = month.berater
 	LEFT JOIN year on berater.berater = year.berater
 	LEFT JOIN stage ON berater.berater = stage.berater
-	LEFT JOIN stage_closing ON berater.berater = stage_closing.berater;
-	
+	LEFT JOIN stage_closing ON berater.berater = stage_closing.berater
+	LEFT JOIN images ON berater.berater = images.berater;
 -- set owner
 ALTER TABLE sc.closings_by_berater_and_time OWNER to read_only;
