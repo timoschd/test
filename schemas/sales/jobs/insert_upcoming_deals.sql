82b677893872d546074741390fc8c2cd9360b45d
Tim M. Schendzielorz <tim.schendzielorz@googlemail.com>
2022-11-29 20:57:58 +0100

    changed schedule

--- schemas/sales/jobs/insert_upcoming_deals.sql
+++ schemas/sales/jobs/insert_upcoming_deals.sql
@@ -1,6 +1,6 @@
 -- make scheduled inserts (jobs in schema cron) for deal stage dash daily
 SELECT cron.schedule('insert_upcoming_deals_daily', -- name
-                     '30 9 * * *', --time
+                     '30 9 * * 1-5', --time
                     'INSERT INTO sc.upcoming_deals_by_time
                         SELECT
                         SUM("Deals"."Betrag 1"::numeric + "Deals"."Betrag 2"::numeric) betrag,
