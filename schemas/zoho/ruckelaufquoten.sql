01e208b730c819728e8fc42ac011acbe70745db3
oliverwzl <oliver.wenzlaff44@gmail.com>
2022-12-05 14:28:25 +0100

    fehler behoben, berechnungen korrigiert

--- schemas/zoho/ruckelaufquoten.sql
+++ schemas/zoho/ruckelaufquoten.sql
@@ -1,69 +1,64 @@
+-- delete
 DROP VIEW IF EXISTS zoho.ruecklaufquoten;
+
+-- create
 CREATE VIEW zoho.ruecklaufquoten AS
---alle respondent ids mit datum und befragungstyp
-with respondents AS (
-SELECT 	survey_respondents.id, 
-		survey_respondents.start_date, 
-		survey_surveys.name
-	FROM zoho.survey_respondents
-	JOIN zoho.survey_surveys
-		ON survey_respondents.survey_id = survey_surveys.id),
---alle jahr/monat mit anzahl der ausgefüllten und befragungstyp
+-- definiere Zeitraum von 08/2022 (Start der befragungen) bis heute + 1 Jahr & cross join alle befragungstypen
+with monat AS (
+SELECT 	to_Char(date_trunc('month', dd)::date, 'YYYY-MM') as monat,
+		CASE 
+			WHEN survey_surveys.name = 'Absolventenbefragung 2022' 
+			THEN 'Absolventenbefragung' 
+			ELSE survey_surveys.name 
+		END as befragung
+	FROM generate_series('2022-08-01'::date, CURRENT_DATE + '1 year'::interval, '1 month'::interval) as dd
+	CROSS JOIN zoho.survey_surveys
+),
+-- select anzahl antworten pro monat, befragung
 antworten AS (
-SELECT 	COUNT(DISTINCT respondent_id) as ausgefuellt, 
-		survey_name,
-		to_char(CAST(datum_spalte as date), 'YYYY-MM') as monat 
-	FROM zoho.evaluationen
-	GROUP BY monat, survey_name
-	ORDER BY monat DESC, survey_name ASC),
---alle jahr/monat mit anzahl der gesendeten und befragungstyp
+SELECT 	COUNT(DISTINCT survey_respondents.id) as antworten,  
+		to_Char(survey_respondents.end_date::date, 'YYYY-MM') as monat, 
+		CASE 
+			WHEN survey_surveys.name = 'Absolventenbefragung 2022' 
+			THEN 'Absolventenbefragung' 
+			ELSE survey_surveys.name 
+		END as befragung	FROM zoho.survey_respondents
+	 JOIN zoho.survey_surveys
+		ON survey_respondents.survey_id = survey_surveys.id
+	GROUP BY befragung, monat
+	ORDER BY monat DESC, befragung ASC
+),
+-- select anzahl gesendete befragungen pro monat, befragung
 gesendet AS (
-SELECT 	COUNT(app_item_id) as anzahl,
+SELECT 	COUNT(DISTINCT app_item_id) as gesendet,
 		to_char((CAST(sende_datum as json)->>'start_date')::date, 'YYYY-MM') as monat,
-		CAST(json_surveydetails as json)->>'SurveyType' as typ
+		CAST(json_surveydetails as json)->>'SurveyType' as befragung
 	FROM podio.evaluation_survey
 	WHERE CAST(status as json)->>'text' = 'Gesendet'
-	GROUP BY monat, typ
-	ORDER BY monat DESC, typ ASC),
---alle jahr/monat mit anzahl der zu sendenden und befragungstyp
-naechste_2 AS (
-SELECT 	COUNT(app_item_id) as anzahl,
+	GROUP BY befragung, monat
+	ORDER BY monat DESC, befragung ASC
+),
+-- select anzahl befragungen welche versendet werden sollen pro monat, befragung
+senden AS (
+SELECT 	COUNT(DISTINCT app_item_id) as senden,
 		to_char((CAST(sende_datum as json)->>'start_date')::date, 'YYYY-MM') as monat,
-		CAST(json_surveydetails as json)->>'SurveyType' as typ
+		CAST(json_surveydetails as json)->>'SurveyType' as befragung
 	FROM podio.evaluation_survey
-	WHERE (CAST(status as json)->>'text' = 'Erstellt' OR CAST(status as json)->>'text' = 'Senden') 
-	GROUP BY monat, typ
-	ORDER BY monat DESC, typ ASC),
---join antworten und gesendete
-join_antworten AS (
-SELECT 	gesendet.monat as jahr_monat,
-		gesendet.typ as befragungstyp,
-		COALESCE(antworten.ausgefuellt, 0) as ausgefuellt,
-		gesendet.anzahl as versendet
-	FROM antworten
-	RIGHT JOIN gesendet
-		ON antworten.monat = gesendet.monat AND antworten.survey_name = gesendet.typ
-	ORDER BY gesendet.monat DESC, typ),
---join nächste monate dazu
-join_naechste AS (
-SELECT 	join_antworten.jahr_monat,
-		join_antworten.befragungstyp,
-		join_antworten.ausgefuellt,
-		join_antworten.versendet,
-		naechste_2.anzahl,
-		naechste_2.monat,
-		naechste_2.typ
-	FROM join_antworten
-	FULL JOIN naechste_2
-	ON join_antworten.jahr_monat = naechste_2.monat AND join_antworten.befragungstyp = naechste_2.typ
-)
-SELECT 	CONCAT(COALESCE(jahr_monat, monat),'-28') as jahr_monat,
-		COALESCE(befragungstyp, typ) as befragungstyp,
-		ausgefuellt,
-		COALESCE(versendet, 0) as versendet,
-		COALESCE(anzahl, 0) as zu_versenden
-	FROM join_naechste
+	WHERE CAST(status as json)->>'text' IN ('Erstellt', 'Senden')
+	GROUP BY befragung, monat
+	ORDER BY monat DESC, befragung ASC)
+-- join ausgefüllt, senden, gesendet an monat/befragung
+SELECT 	monat.monat as jahr_monat, 
+		monat.befragung as befragungstyp, 
+		COALESCE(antworten.antworten, 0) as ausgefuellt,
+		COALESCE(gesendet.gesendet, 0) as versendet,
+		COALESCE(senden.senden, 0) as zu_versenden
+	FROM monat 
+	LEFT JOIN antworten ON monat.monat = antworten.monat AND monat.befragung = antworten.befragung
+	LEFT JOIN gesendet ON monat.monat = gesendet.monat AND monat.befragung = gesendet.befragung
+	LEFT JOIN senden ON monat.monat = senden.monat AND monat.befragung = senden.befragung
 	ORDER BY jahr_monat, befragungstyp;
 	
+-- setze owner		
 ALTER TABLE zoho.ruecklaufquoten 
 	OWNER TO read_only;
